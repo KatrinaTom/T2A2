a3bbaca546a079cbde62dd07b3d1c8306acc48f1
Katrina Tomaszcyk <katrina.tomaszcyk@192-168-1-106.tpgi.com.au>
2022-11-02 15:16:44 +1000

    first successful table created

--- src/main.py
+++ src/main.py
@@ -1,18 +1,44 @@
 # List of all the imports. Main working file is main.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 # Flask creation
 app = Flask(__name__)
 
-# app configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://landscaping_admin_db"
+# app configuration to set the databse URI via SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
-# Instances created
+# Create the database object
 db = SQLAlchemy(app)
 
+# CLI Commands 
+@app.cli.command("create")
+def create_db():
+    db.create_all()
+    print("Tables created successfully")
 
-# Routes created
+@app.cli.command("drop")
+def drop_db():
+    db.drop_all()
+    print("Tables dropped successfully")
+
+
+# Database model - Users Table
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False)
+    f_name = db.Column(db.String(128), nullable=False)
+    l_name = db.Column(db.String(128), nullable=False)
+    address = db.Column(db.String, nullable=False)
+    p_number = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+
+
+
+# Routes created - Test route
 @app.route('/')
 def index():
     return "Hello There!"
